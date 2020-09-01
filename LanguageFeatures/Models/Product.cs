ae34eb9b69d430ca329d213e3601c9e0487fd5d9
DmitryAngarsky <53812480+DmitryAngarsky@users.noreply.github.com>
2020-09-01 21:00:07 +0400

    LanguageFeatures.Stage-2
    Using auto-enforceable properties

--- LanguageFeatures/Models/Product.cs
+++ LanguageFeatures/Models/Product.cs
@@ -1,21 +1,30 @@
 ﻿using System;
+
 namespace LanguageFeatures.Models
 {
     public class Product
     {
+        public Product(bool stock = true)
+        {
+            InStock = stock;
+        }
+
         public string Name { get; set; }
+        public string Category { get; set; } = "Watersports";
         public decimal? Price { get; set; }
         public Product Releated { get; set; }
+        public bool InStock { get; }
 
         public static Product[] GetProducts()
         {
             Product kayak = new Product
             {
                 Name = "Kayak",
+                Category = "Water Craft",
                 Price = 250M
             };
 
-            Product lifejacket = new Product
+            Product lifejacket = new Product(false)
             {
                 Name = "Lifejacket",
                 Price = 48.95M
