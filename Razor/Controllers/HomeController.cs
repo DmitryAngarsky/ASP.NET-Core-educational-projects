faf7d79deaea35bfb526a8e1a50041af1218af62
DmitryAngarsky <53812480+DmitryAngarsky@users.noreply.github.com>
2020-09-03 22:19:48 +0400

    Razor.Stage-0(initial)
    Project preparation

---
+++ Razor/Controllers/HomeController.cs
@@ -0,0 +1,23 @@
+﻿
+using Microsoft.AspNetCore.Mvc;
+using Razor.Models;
+
+namespace Razor.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ViewResult Index()
+        {
+            Product myProduct = new Product
+            {
+                ProductID = 1,
+                Name = "Kayak",
+                Description = "A boat for one person",
+                Category = "Watersports",
+                Price = 275M
+            };
+
+            return View(myProduct);
+        }
+    }
+}
