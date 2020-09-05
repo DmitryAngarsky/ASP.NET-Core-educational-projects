67f26caf544ed9c32783fc6e82a9716239c3958b
DmitryAngarsky <53812480+DmitryAngarsky@users.noreply.github.com>
2020-09-05 23:37:44 +0400

    Razor.Stage-1(final)

--- Razor/Controllers/HomeController.cs
+++ Razor/Controllers/HomeController.cs
@@ -6,18 +6,17 @@ namespace Razor.Controllers
 {
     public class HomeController : Controller
     {
-        public ViewResult Index()
+        public IActionResult Index()
         {
-            Product myProduct = new Product
+            Product[] array =
             {
-                ProductID = 1,
-                Name = "Kayak",
-                Description = "A boat for one person",
-                Category = "Watersports",
-                Price = 275M
+                new Product {Name = "Kayak", Price = 275M},
+                new Product {Name = "Lifejacket", Price = 48.95M},
+                new Product {Name = "Soccer ball", Price = 19.50M},
+                new Product {Name = "Corner flag", Price = 34.95M}
             };
 
-            return View(myProduct);
+            return View(array);
         }
     }
 }
--- Razor/Models/Product.cs
+++ Razor/Models/Product.cs
@@ -1,4 +1,4 @@
-﻿using System;
+﻿
 namespace Razor.Models
 {
     public class Product
