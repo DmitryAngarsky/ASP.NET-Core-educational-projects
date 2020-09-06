5777ef1fda92b60f8ea23c9350905fdf3371c975
DmitryAngarsky <53812480+DmitryAngarsky@users.noreply.github.com>
2020-09-06 21:32:49 +0400

    WоrkingWithVisualStudio.Stage-1
    Preparation for unit tests

--- WоrkingWithVisualStudio/Controllers/HomeController.cs
+++ WоrkingWithVisualStudio/Controllers/HomeController.cs
@@ -9,7 +9,19 @@ namespace WоrkingWithVisualStudio.Controllers
 {
     public class HomeController : Controller
     {
+        SimpleRepository Repository = SimpleRepository.SharedRepository;
+
         public IActionResult Index() => View(SimpleRepository.SharedRepository.Products
             .Where(p => p?.Price < 50));
+
+        [HttpGet]
+        public IActionResult AddProduct() => View(new Product());
+
+        [HttpPost]
+        public IActionResult AddProduct(Product p)
+        {
+            Repository.AddProduct(p);
+            return RedirectToAction("Index");
+        }
     }
 }
