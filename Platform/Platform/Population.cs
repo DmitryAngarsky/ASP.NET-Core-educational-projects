7a3c824a6eddcbb1053bc8c28a73740832ebfe3a
DmitryAngarsky <53812480+DmitryAngarsky@users.noreply.github.com>
2020-11-20 23:49:53 +0400

    URL Routing(1)

---
+++ Platform/Platform/Population.cs
@@ -0,0 +1,44 @@
+using System;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+
+namespace Platform
+{
+    public class Population
+    {
+        private readonly RequestDelegate _next;
+
+        public Population()
+        {
+            
+        }
+
+        public Population(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public async Task Invoke(HttpContext context)
+        {
+            string[] parts = context.Request.Path.ToString().Split("/", StringSplitOptions.RemoveEmptyEntries);
+            if (parts.Length == 2 && parts[1] == "population")
+            {
+                string city = parts[0];
+
+                int? pop = city.ToLower() switch
+                {
+                    "london" => 8_136_000,
+                    "paris" => 2_141_000,
+                    "monaco" => 39_000,
+                    _ => null
+                };
+
+                if (pop.HasValue)
+                    await context.Response.WriteAsync($"City: {city}, Population: {pop}");
+            }
+
+            if (_next != null)
+                await _next(context);
+        }
+    }
+}
